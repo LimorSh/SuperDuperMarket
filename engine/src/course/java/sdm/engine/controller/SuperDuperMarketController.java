b888f8cde86ade598e421caff0620f242867019b
LimorSh <limorshenkar@gmail.com>
2020-08-25 19:58:18 +0300

    Started Ex2.

---
+++ engine/src/course/java/sdm/engine/controller/SuperDuperMarketController.java
@@ -0,0 +1,65 @@
+package course.java.sdm.engine.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SuperDuperMarketController {
+
+    @FXML
+    private Button customersButton;
+
+    @FXML
+    private Button storesButton;
+
+    @FXML
+    private Button productsButton;
+
+    @FXML
+    private Button ordersButton;
+
+    @FXML
+    private Button loadFileButton;
+
+    @FXML
+    private Button updateItemButton;
+
+    @FXML
+    private Button addOrderButton;
+
+    @FXML
+    void addOrderButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customersButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadFileButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ordersButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void productsButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void storesButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateItemButtonAction(ActionEvent event) {
+
+    }
+
+}
