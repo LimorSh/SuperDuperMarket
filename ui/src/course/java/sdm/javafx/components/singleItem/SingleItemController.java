a0a6d65e8cd558be0e2910f39b27d4c99d83d26b
LimorSh <limorshenkar@gmail.com>
2020-08-31 21:45:37 +0300

    Created single item.

---
+++ ui/src/course/java/sdm/javafx/components/singleItem/SingleItemController.java
@@ -0,0 +1,20 @@
+package course.java.sdm.javafx.components.singleItem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SingleItemController {
+
+    @FXML private Label idLabel;
+
+    @FXML private Label nameLabel;
+
+    @FXML private Label purchaseCategoryLabel;
+
+    @FXML private Label numberOfStoresSellingTheItemLabel;
+
+    @FXML private Label averagePriceLabel;
+
+    @FXML private Label totalSellsLabel;
+
+}
